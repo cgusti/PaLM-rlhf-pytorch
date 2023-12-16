685c2bb5c2bef3a32a707e92370436e2715f6195
EC2 Default User <ec2-user@ip-172-16-93-215.us-east-2.compute.internal>
2023-12-16 00:21:48 +0000

    added docstring

--- seq2seq/reward_model.py
+++ seq2seq/reward_model.py
@@ -1,10 +1,9 @@
 '''
-Our reward model should accunt for multiple factors: 
+Our reward model for optimizing resource utilization should account for multiple factors: 
 
 - Diagnostic accuracy: You want to minimize over-testing and under-testing, so the reward should encourage ordering tests that provide useful diagnostic information.
 - Cost: Consider the cost of ordering tests. Minimize the total cost, which includes the cost of individual tests as well as any penalties for over-testing.
 - Rare Occurrence Penalty: Assign higher penalties to larger order sets since they indicate inefficient resource allocation. This could be a term that increases as the number of tests ordered increases.
-- Resilience: Consider incorporating a term that encourages resilience to environmental factors. This could involve penalizing actions that are sensitive to environmental fluctuations.
 '''
 
 import torch 
