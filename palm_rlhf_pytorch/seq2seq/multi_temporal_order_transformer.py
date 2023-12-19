76d59cfcc610280c99c8480499b620d9c77bb373
EC2 Default User <ec2-user@ip-172-16-93-215.us-east-2.compute.internal>
2023-12-19 23:41:10 +0000

    added docstring and additional comments to greedy decoder

--- palm_rlhf_pytorch/seq2seq/multi_temporal_order_transformer.py
+++ palm_rlhf_pytorch/seq2seq/multi_temporal_order_transformer.py
@@ -186,6 +186,9 @@ class Seq2SeqTransformer(nn.Module):
     #                                     tgt_mask)
 
 def greedy_decode(model, src_ord, src_res, pat_cov, max_len, BOS_idx, EOS_idx):
+    '''
+    autoregressively generate tokens using greedy decoding 
+    '''
     model.eval()
 
     src_pat_emb = model.pat_cov_emb(pat_cov)
@@ -193,27 +196,24 @@ def greedy_decode(model, src_ord, src_res, pat_cov, max_len, BOS_idx, EOS_idx):
     src_emb = torch.add(torch.mul(model.alpha_o, model.src_ord_emb(src_ord)), 
                         torch.mul(model.alpha_r, model.src_res_emb(src_res)))  
     src_emb = torch.add(src_emb, src_pat_emb) # (80, 1, 256)
-    print(f"src_emb: {src_emb.shape}") 
     memory = model.transformer.encoder(src_emb) # (80, 1, 256)
-    print(f"memory: {memory.shape}") 
 
     # Start the output tensor with the start symbol
     ys = torch.ones(1, 1).fill_(BOS_idx).type_as(src_ord.data)
-    print(f"ys: {ys.shape}")
 
     for i in range(max_len-1):
         #Decode one step at a time 
         tgt_emb = model.tgt_tok_emb(ys)
-        print(f"tgt_emb: {tgt_emb.shape}")
         out = model.transformer.decoder(tgt_emb, memory)
         out = out.transpose(0,1)
-        prob = model.generator(out[:, -1])
+        #pass embeddings through generator to get out logits
+        prob = model.generator(out[:, -1]) 
         _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.item()
+        #convert to python int
+        next_word = next_word.item() #convert to python int
 
         #Append the predicted word to the output sequence
         ys = torch.cat([ys, torch.ones(1, 1).fill_(next_word).type_as(src_ord.data)], dim=0)
-        print(f"iteration{i}: {ys.shape}")
 
         #Break if EOS token is predicted
         if next_word == EOS_idx: 
